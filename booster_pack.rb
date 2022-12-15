dccb535fc76460de6ab8c072b3049cf1b8e77489
Josh Mills <josh@trueheart78.com>
2022-12-15 11:08:49 -0500

    Update to booster_pack.rb

--- booster_pack.rb
+++ booster_pack.rb
@@ -4,6 +4,5 @@ require_relative 'init/environment'
 require_relative 'init/bundler'
 require_relative 'init/zeitwerk'
 
-# Require any stdlib libraries here
-require 'date'
-require 'json'
+# Require stdlib libraries here
+# require 'json'
