585eb1309cbb58e77e14bacaa8aee5cbe95c2116
Josh Mills <josh@trueheart78.com>
2022-12-15 11:20:20 -0500

    Fixed RuboCop errors :robot:

--- init/zeitwerk.rb
+++ init/zeitwerk.rb
@@ -4,7 +4,7 @@ require 'zeitwerk'
 
 loader = Zeitwerk::Loader.new
 
-root_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
+root_dir = File.expand_path(File.dirname(__FILE__, 2))
 
 loader.push_dir(File.join(root_dir, 'lib'))
 
--- spec/linters/rubocop_spec.rb
+++ spec/linters/rubocop_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe 'RuboCop' do
   subject(:status) { system('rubocop --fail-fast --config .rubocop.yml >/dev/null 2>&1') }
 
   it 'passes' do
-    expect(status).to eq true
+    expect(status).to be true
   end
 end
